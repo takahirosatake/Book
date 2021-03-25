2ce7954bc2b2b6d3f146cd6d6a6a3d038d5e9d12
ponsata <khr.04237@gmail.com>
2021-03-26 01:14:13 +0900

    commit

---
+++ src/global/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>    
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar-title to="/">The Books</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+      color="error"
+      @click= "deleteLocalStorage">
+      削除する
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    methods:{
+      deleteLocalStorage(){
+        this.$emit('delete-local-storage')
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import BookIndex from '@/pages/BookIndex'
+import BookSearch from '@/pages/BookSearch'
+import BookEdit from '@/pages/BookEdit'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'BookIndex',
+    component: BookIndex
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/search',
+    name: 'BookSearch',
+    component: BookSearch
+  },
+  {
+    path: '/edit/:id',
+    name: 'BookEdit',
+    component: BookEdit
+  },
+  {
+    path: '*', //存在しないルーティングはルートパスの移動させる
+    redirect: '/',
+ },
+
+
 ]
 
 const router = new VueRouter({
